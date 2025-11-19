4bd0110d1577f3aa003b9eca072db7d9bb634032
Phat Lee <vanphat15it@gmail.com>
2025-11-19 10:33:39 +0700

    fix: Remove default password from MySQL connection configuration

--- src/config/database.js
+++ src/config/database.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 const sequelize = new Sequelize(
   process.env.DB_NAME || "trackifi_dev",
   process.env.DB_USER || "root",
-  process.env.DB_PASSWORD || "123456",
+  process.env.DB_PASSWORD || "",
   {
     host: process.env.DB_HOST || "localhost",
     port: process.env.DB_PORT || 3306,
